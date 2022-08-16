2684a7b78d2941731372df536d9ec56ac3194245
rida-el <rida.el99@gmail.com>
2022-08-16 14:51:09 +0100

    fix the particle

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import hero from "./Componants/img/hero.png";
+import Navbar from "./Componants/Navbar/Navbar";
+import Strategie from "./Componants/strategie/Strategie";
+import Project from "./Componants/Projects/Project";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section
+      className="bg-url h-screen bg-Hero bg-cover
+    font-[Poppins] md:bg-top bg-center"
+    >
+      <Navbar />
+      <div className="flex flex-col justify-center text-center items-center h-3/4 relative">
+        {/* <h2 className="text-2xl font-medium ">Fashion Tips</h2> */}
+        <h1 className="font md:text-5xl text-3xl py-1 text-white absolute top-28 left-40">
+          explorons les limites du <span className="sous-font">digital</span>
+        </h1>
+        <h1 className="font md:text-5xl text-3xl py-1 text-white absolute top-20 left-20">
+          Entre <span className="sous-font">projets</span> et{" "}
+          <span className="sous-font">innovation</span>
+        </h1>
+        <div className="">
+          <img
+            className="hero-size absolute top-20 right-0 "
+            src={hero}
+            alt=""
+          />
+          <p className="text-3xl rot absolute top-30">SCROL</p>
+        </div>
+      </div>
+      <Strategie />
+      <Project />
+    </section>
+    
   );
-}
+};
 
 export default App;
---
+++ src/Componants/Button.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Button = () => {
+  return (
+    <button className="bg-indigo-600 text-white py-2 px-6 rounded md:ml-8 hover:bg-indigo-400 duration-500">Get Started</button>
+  );
+};
+
+export default Button;
