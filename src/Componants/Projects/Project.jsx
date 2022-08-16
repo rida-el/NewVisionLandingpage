62f0aa93ee15399e5f0c3f6e6a90717835b6c51d
rida-el <rida.el99@gmail.com>
2022-08-16 14:52:25 +0100

    add project section

---
+++ src/Componants/Projects/Project.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./project.css";
+
+export default function Project() {
+  return (
+    <div className="h-screen bg flex flex-col justify-center items-center">
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto sed
+      facilis molestiae ad laudantium. Dignissimos quia, modi omnis a ullam
+      tenetur atque quibusdam libero, sequi architecto enim soluta doloribus
+      culpa.
+      
+    </div>
+  );
+}
