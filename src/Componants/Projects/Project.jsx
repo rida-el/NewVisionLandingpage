af62f9141f9ad945d14602652e423c7b5497087a
rida-el <rida.el99@gmail.com>
2022-08-16 18:52:35 +0100

    fix some changes in project section

--- src/Componants/Projects/Project.jsx
+++ src/Componants/Projects/Project.jsx
@@ -1,14 +1,26 @@
 import React from "react";
+import mockup from "../img/mockup.png";
 import "./project.css";
 
 export default function Project() {
   return (
     <div className="h-screen bg flex flex-col justify-center items-center">
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto sed
-      facilis molestiae ad laudantium. Dignissimos quia, modi omnis a ullam
-      tenetur atque quibusdam libero, sequi architecto enim soluta doloribus
-      culpa.
-      
+      <div className="flex tx-pro">
+        <div className="flex justify-center items-center max-w-[40%] px-[10%]">
+          <img className="mockup-img" src={mockup} alt="" />
+        </div>
+        <div className="flex flex-col items-left max-w-[30%] items-start ">
+          <h1 className="font-t1">Projets &</h1>
+          <h1 className="font-t2">ÉTUDES DE CAS</h1>
+          <div className="pt-[10%]">
+            <p className="font-p">
+              Découvrez nos projets ainsi qu'une sélection d'études de cas,
+              apportant une réponse digitale réfléchie et adaptée à des
+              problématiques aux enjeux variés.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/Componants/strategie/Strategie.jsx
+++ src/Componants/strategie/Strategie.jsx
@@ -1,29 +1,29 @@
 import React, { Component } from "react";
-import illus from "../img/file.png"
+import illus from "../img/file.svg";
 import "./strategie.css";
 
 function Strategie() {
   return (
-    <div>
-      <div className="h-screen mt-10 flex flex-col justify-center text-center items-center relative">
-        <img src={illus} />
-        <div className="flex flex-row">
-          <span className="title-logo">#</span>
-          <h1 className="strategie-font">stratégie digitale</h1>
-        </div>
-        <div className="flex flex-col sta-texte">
-          <p>Créons ensemble une expérience digitale</p>
-          <p>
-            <span className="sta-texte-bold">intelligente</span> et{" "}
-            <span className="sta-texte-bold">performante</span>
-          </p>
-        </div>
-        <div className="sta-button">
-          <a href=""> DÉCOUVRIR NOUS </a>
-        </div>
-
-        {/* <div className="pagination">01</div> */}
+    <div className="h-screen justify-start flex flex-col text-center items-center relative">
+      <img className="h-[30%]" src={illus} />
+      <div className="flex flex-row">
+        <span className="title-logo">#</span>
+        <h1 className="strategie-font">stratégie digitale</h1>
       </div>
+      <div className="flex flex-col sta-texte">
+        <p>Créons ensemble une expérience digitale</p>
+        <p>
+          <span className="sta-texte-bold">intelligente</span> et{" "}
+          <span className="sta-texte-bold">performante</span>
+        </p>
+      </div>
+      <div className="sta-button">
+        <a href=""> DÉCOUVRIR NOUS </a>
+      </div>
+      <div className="text-hor text-hor-border">
+        <p>NOUS CRÉONS DES EXPÉRIENCES DIGITALES</p>
+      </div>
+      {/* <div className="pagination">01</div> */}
     </div>
   );
 }
